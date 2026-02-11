9a21c8d23a1cd696da8de75c79d5cb77c952e80b
ravipandey <ravipandey.ciitm@gmail.com>
2026-02-11 09:55:00 +0530

    Add or update Validators/UploadMsgFileValidator.cs

---
+++ Validators/UploadMsgFileValidator.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+
+public class UploadMsgFileModel
+{
+    public IFormFile MsgFile { get; set; } = null!;
+}
+
+public class UploadMsgFileValidator : AbstractValidator<UploadMsgFileModel>
+{
+    public UploadMsgFileValidator()
+    {
+        RuleFor(x => x.MsgFile)
+            .NotNull()
+            .Must(f => f.FileName.EndsWith(".msg", StringComparison.OrdinalIgnoreCase))
+            .WithMessage("File must be a .msg file");
+    }
+}
+```
+
+---
+
+### 6. API Controller
+
+```csharp
